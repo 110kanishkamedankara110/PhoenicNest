ee9251d9844b4f12db6bb7efa7622d4c18d2f030
kanishka medankara <98632131+110kanishkamedankara110@users.noreply.github.com>
2023-11-22 17:29:51 +0530

    button style changed

--- app/src/main/java/com/phoenix/phoenixNest/SingleViewFragment.java
+++ app/src/main/java/com/phoenix/phoenixNest/SingleViewFragment.java
@@ -9,6 +9,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+import com.phoenix.phoenicnest.R;
 
 
 public class SingleViewFragment extends Fragment {
@@ -31,12 +35,14 @@ public class SingleViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(com.phoenix.phoenicnest.R.layout.fragment_single_view, container, false);
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        ImageView iv=view.findViewById(R.id.imageView2);
 
 
 
